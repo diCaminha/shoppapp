e904311ed81e9a90b82a9b4001cf167290a762a3
denis <dineresc@gmail.com>
2019-08-29 19:30:38 -0300

    more details in item of list of produts

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -22,7 +22,20 @@ class ProductItem extends StatelessWidget {
         imageUrl,
         fit: BoxFit.cover,
       ),
-      footer: GridTileBar(title: Text(title, textAlign: TextAlign.center,),),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        leading: IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: (){},
+        ),
+        title: Text(title, 
+        textAlign: TextAlign.center,
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.shopping_cart),
+          onPressed: (){},
+        ),
+      ),
     );
   }
 }
